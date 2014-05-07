51c496ebb9dd1619fa6e1aa4aeea51eea7455050
Dave Eddy <dave@daveeddy.com>
2014-05-06 22:28:05 -0400

    Update stretch.js

--- stretch.js
+++ stretch.js
@@ -4,7 +4,7 @@ function stretch(arr, size) {
   var arrlen = arr.length;
   var ret = [];
   for (var i = 0; i < arrlen; i += arrlen / size) {
-    // we have to some stupid bullshit here to deal with the situation where the
+    // we have to do some stupid bullshit here to deal with the situation where the
     // step is .333; it will end doing something dumb like:
     // 0.33, 0.66, 1, 1.33, 1.66, 1.99, 2.33, etc. so let's get dirty
     if ((i - Math.floor(i)) > 0.999)
